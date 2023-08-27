726b46f9c96af942aa649acb917142f45bb65754
saeedibrahem <s3eedeb@gmail.com>
2023-08-27 05:00:53 +0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,62 +9,32 @@ const callBack = async (url) => {
   }
   return await data;
 };
-callBack("../products.json")
-  .then((res) => {
+callBack("../products.json").then((res) => {
     const products = document.querySelector(".products-slider");
     products.innerHTML = "";
     res.products.forEach((product) => {
-      products.innerHTML += `
-    <div class="item">
+      products.innerHTML += `<div class="item">
           <div class="position-relative || imgProduct">
-            <img
-              src=${product.image[0]}
-              alt=""
-              class="img1"
-            />
-            ${
-              product.image[1]
-                ? ` <img
-            src=${product.image[1]}
-            alt=""
-            class="img2"
-          />`
-                : ` <img
-          src=${product.image[0]}
-          alt=""
-          class="img2"
-        />`
-            }
+            <img src=${product.image[0]}  alt=""    class="img1"/>
+              ${    product.image[1] ? `<img src=${product.image[1]} alt="" class="img2"/>`   : ` <img src=${product.image[0]} alt="" class="img2"/>` }
             </div>
-          <div class="product__info">
-          <div class="position-relative">
-          <span class="hint--top hint--medium position-absolute w-100 z-1 hintPos"  aria-label="${
-            product.name.ar
-          }">
-          <span class="opacity-0">${product.name.ar}</span>
-          </span>
+            <div class="product__info">
+            <div class="position-relative">
+            <span class="hint--top hint--medium position-absolute w-100 z-1 hintPos"  aria-label="${product.name.ar}">
+            <span class="opacity-0">${product.name.ar}</span>
+            </span>
             <h3 class="overLap">${product.name.ar}</h3>
           </div>
             <p>
             <span class="new__price">$${product.price}</span>
             <span class="old__price">$${product.old_price}</span>
             </p>
-            <div class="colors">
-            ${product.colors
-              .map(
-                (color) =>
-                  ` <div style="background-color: ${color};" class="black__color"></div>`
-              )
-              .join("")}
-              </div>
-          </div>
+            <div class="colors">${product.colors.map((color) =>` <div style="background-color: ${color};" class="black__color"></div>`).join("")}</div></div>
         </div>
-    
-    `;
+        `;
     });
   })
-  .then((r) => {
-    $(".owl-carousel.products-slider").owlCarousel({
+  .then(() => {$(".owl-carousel.products-slider").owlCarousel({
       loop: true,
       rtl: true,
       // autoplay: true,
@@ -149,6 +119,7 @@ const closeModal = (e) => {
   document.querySelector("body").style.overflow = "auto";
   document.querySelector("body").style.paddingRight = "0";
 };
+
 const popupBtns = document.querySelectorAll(".popup-event");
 popupBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -167,7 +138,8 @@ popupBtns.forEach((btn) => {
 document.addEventListener("keydown", (event) => {
   const key = event.key;
   if (key === "Escape") {
-    closeModal("menu");
+    closeModal("category__menu");
+    closeModal("cart__menu");
     closeModal("login");
   }
 });
@@ -179,27 +151,31 @@ const userData = JSON.parse(localStorage.getItem("userData")) ?? [];
 
 loginBtn.addEventListener("click", () => {
   loginForm.classList.add("active");
-  registerBtn.classList.add("btn-light");
-  registerBtn.classList.remove("btn-dark");
-  loginBtn.classList.add("btn-dark");
+  activeBtn(registerBtn ,loginBtn )
+  // registerBtn.classList.add("btn-light");
+  // registerBtn.classList.remove("btn-dark");
+  // loginBtn.classList.add("btn-dark");
 });
 registerBtn.addEventListener("click", () => {
   loginForm.classList.remove("active");
-  loginBtn.classList.remove("btn-dark");
-  loginBtn.classList.add("btn-light");
-  registerBtn.classList.add("btn-dark");
+  activeBtn( loginBtn , registerBtn)
+  // loginBtn.classList.add("btn-light");
+  // loginBtn.classList.remove("btn-dark");
+  // registerBtn.classList.add("btn-dark");
 });
+
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   if (e.target.classList.contains("active")) {
     const findme = userData.find((ele) => ele.email === e.target.email.value);
-    console.log(findme);
     if (findme) {
       if (findme.password === e.target.password.value) {
-        localStorage.setItem("login", JSON.stringify(findme));
+        sendDataToLocalStotage("login", findme);
+        // localStorage.setItem("login", JSON.stringify(findme));
         closeModal("login");
         document.querySelector("body").classList.add("isLogin");
+        clearInputValue(e);
       }
     }
   } else {
@@ -210,18 +186,35 @@ loginForm.addEventListener("submit", (e) => {
     const findme = userData.find((e) => e.email === newUser.email);
     if (!findme) {
       userData.push(newUser);
-      localStorage.setItem("userData", JSON.stringify(userData));
+      sendDataToLocalStotage("userData", userData);
+      // localStorage.setItem("userData", JSON.stringify(userData));
       loginForm.classList.add("active");
-      registerBtn.classList.add("btn-light");
-      registerBtn.classList.remove("btn-dark");
-      loginBtn.classList.add("btn-dark");
-      e.target.email.value = "";
-      e.target.password.value = "";
+      activeBtn(registerBtn , loginBtn)
+      // registerBtn.classList.add("btn-light");
+      // registerBtn.classList.remove("btn-dark");
+      // loginBtn.classList.add("btn-dark");
+      clearInputValue(e);
     }
   }
 });
-
 document.querySelector(".logOut").addEventListener("click", () => {
   localStorage.removeItem("login");
   document.querySelector("body").classList.remove("isLogin");
 });
+
+const clearInputValue = (e) => {
+  e.target.email.value = "";
+  e.target.password.value = "";
+};
+
+const sendDataToLocalStotage = (target, data) => {
+  return localStorage.setItem(target, JSON.stringify(data));
+};
+const getDataFromLocalStotage = (data) => {
+  return JSON.parse(localStorage.getItem(data))
+};
+const activeBtn = (btn1 , btn2)=>{
+  btn1.classList.add("btn-light");
+  btn1.classList.remove("btn-dark");
+  btn2.classList.add("btn-dark");
+}
