3c692a28714be88cd906860dacceefe8bd8ec7bf
SamehElsaid30 <Sameh@ensegypt.com>
2023-08-27 22:06:48 +0300

    edit some section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
-// import { products } from "./products.js";
+let renderedProduct = [];
+
 const callBack = async (url) => {
   let data = null;
   try {
@@ -9,18 +10,26 @@ const callBack = async (url) => {
   }
   return await data;
 };
-callBack("../products.json").then((res) => {
+callBack("../products.json")
+  .then((res) => {
+    renderedProduct = res.products;
     const products = document.querySelector(".products-slider");
     products.innerHTML = "";
     res.products.forEach((product) => {
       products.innerHTML += `<div class="item">
           <div class="position-relative || imgProduct">
             <img src=${product.image[0]}  alt=""    class="img1"/>
-              ${    product.image[1] ? `<img src=${product.image[1]} alt="" class="img2"/>`   : ` <img src=${product.image[0]} alt="" class="img2"/>` }
+              ${
+                product.image[1]
+                  ? `<img src=${product.image[1]} alt="" class="img2"/>`
+                  : ` <img src=${product.image[0]} alt="" class="img2"/>`
+              }
             </div>
             <div class="product__info">
             <div class="position-relative">
-            <span class="hint--top hint--medium position-absolute w-100 z-1 hintPos"  aria-label="${product.name.ar}">
+            <span class="hint--top hint--medium position-absolute w-100 z-1 hintPos"  aria-label="${
+              product.name.ar
+            }">
             <span class="opacity-0">${product.name.ar}</span>
             </span>
             <h3 class="overLap">${product.name.ar}</h3>
@@ -29,12 +38,18 @@ callBack("../products.json").then((res) => {
             <span class="new__price">$${product.price}</span>
             <span class="old__price">$${product.old_price}</span>
             </p>
-            <div class="colors">${product.colors.map((color) =>` <div style="background-color: ${color};" class="black__color"></div>`).join("")}</div></div>
+            <div class="colors">${product.colors
+              .map(
+                (color) =>
+                  ` <div style="background-color: ${color};" class="black__color"></div>`
+              )
+              .join("")}</div></div>
         </div>
         `;
     });
   })
-  .then(() => {$(".owl-carousel.products-slider").owlCarousel({
+  .then(() => {
+    $(".owl-carousel.products-slider").owlCarousel({
       loop: true,
       rtl: true,
       // autoplay: true,
@@ -69,37 +84,6 @@ callBack("../products.json").then((res) => {
     });
   });
 
-//     .then((res) => {
-//         const card__container = document.querySelector(".card__container");
-//         card__container.innerHTML = "";
-//         if (res) {
-//             res.forEach((product) => {
-//                 card__container.innerHTML += `
-//             <div class="col-3 p-2">
-//             <div class="card">
-//                 <img src=${product.image} class="card-img-top"/>
-//                 <div class="card-body">
-//                 <h5 class="card-title">${product.title}</h5>
-//                 <p class="card-price">$${product.price}</p>
-//                 <p class="card-text">${product.category}</p>
-//                 <a href="/product.html?${product.id}" class="btn btn-primary">show product</a>
-//                 </div>
-//                 q</div>
-//             </div> `;
-//             });
-//         } else {
-//             card__container.innerHTML = `
-//         <h2> this page is not found</h2>
-//         <button onClick="reloadPage()" class="btn btn-primary">reload page</button>`;
-//         }
-//     })
-//     .finally((_) => {
-//         const loading = document.querySelector(".loading").classList.add("done");
-//     });
-
-// function reloadPage() {
-//     window.location.reload();
-// }
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".loading").classList.add("done");
   if (JSON.parse(localStorage.getItem("login"))) {
@@ -108,16 +92,13 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("body").classList.remove("isLogin");
   }
 });
-// const toggleMenu = document.querySelector(".navbar__toggle-menu");
-// const modalContainer = document.querySelector(".modal-container");
-
-// toggleMenu.addEventListener("click", () => {});
 
 const closeModal = (e) => {
   let element = document.getElementById(e);
   element.classList.remove("active");
   document.querySelector("body").style.overflow = "auto";
   document.querySelector("body").style.paddingRight = "0";
+  document.querySelector(".new__navList-container").classList.remove("pop");
 };
 
 const popupBtns = document.querySelectorAll(".popup-event");
@@ -128,9 +109,7 @@ popupBtns.forEach((btn) => {
     if (element.classList.contains("active")) {
       document.querySelector("body").style.overflow = "hidden";
       document.querySelector("body").style.paddingRight = "17px";
-    } else {
-      document.querySelector("body").style.overflow = "auto";
-      document.querySelector("body").style.paddingRight = "0";
+      document.querySelector(".new__navList-container").classList.add("pop");
     }
   });
 });
@@ -143,7 +122,6 @@ document.addEventListener("keydown", (event) => {
     closeModal("login");
   }
 });
-
 const loginBtn = document.querySelector(".login-title button:nth-child(1)");
 const registerBtn = document.querySelector(".login-title button:nth-child(2)");
 const loginForm = document.querySelector(".box__container form");
@@ -151,17 +129,11 @@ const userData = JSON.parse(localStorage.getItem("userData")) ?? [];
 
 loginBtn.addEventListener("click", () => {
   loginForm.classList.add("active");
-  activeBtn(registerBtn ,loginBtn )
-  // registerBtn.classList.add("btn-light");
-  // registerBtn.classList.remove("btn-dark");
-  // loginBtn.classList.add("btn-dark");
+  activeBtn(registerBtn, loginBtn);
 });
 registerBtn.addEventListener("click", () => {
   loginForm.classList.remove("active");
-  activeBtn( loginBtn , registerBtn)
-  // loginBtn.classList.add("btn-light");
-  // loginBtn.classList.remove("btn-dark");
-  // registerBtn.classList.add("btn-dark");
+  activeBtn(loginBtn, registerBtn);
 });
 
 loginForm.addEventListener("submit", (e) => {
@@ -172,7 +144,6 @@ loginForm.addEventListener("submit", (e) => {
     if (findme) {
       if (findme.password === e.target.password.value) {
         sendDataToLocalStotage("login", findme);
-        // localStorage.setItem("login", JSON.stringify(findme));
         closeModal("login");
         document.querySelector("body").classList.add("isLogin");
         clearInputValue(e);
@@ -187,12 +158,10 @@ loginForm.addEventListener("submit", (e) => {
     if (!findme) {
       userData.push(newUser);
       sendDataToLocalStotage("userData", userData);
-      // localStorage.setItem("userData", JSON.stringify(userData));
+
       loginForm.classList.add("active");
-      activeBtn(registerBtn , loginBtn)
-      // registerBtn.classList.add("btn-light");
-      // registerBtn.classList.remove("btn-dark");
-      // loginBtn.classList.add("btn-dark");
+      activeBtn(registerBtn, loginBtn);
+
       clearInputValue(e);
     }
   }
@@ -210,11 +179,96 @@ const clearInputValue = (e) => {
 const sendDataToLocalStotage = (target, data) => {
   return localStorage.setItem(target, JSON.stringify(data));
 };
-const getDataFromLocalStotage = (data) => {
-  return JSON.parse(localStorage.getItem(data))
-};
-const activeBtn = (btn1 , btn2)=>{
+// const getDataFromLocalStotage = (data) => {
+//   return JSON.parse(localStorage.getItem(data))
+// };
+const activeBtn = (btn1, btn2) => {
   btn1.classList.add("btn-light");
   btn1.classList.remove("btn-dark");
   btn2.classList.add("btn-dark");
-}
+};
+
+setInterval(() => {
+  const now = new Date("Aug 30,2023").getTime();
+  const now2 = new Date().getTime();
+  const timedays = now - now2;
+  const hoursnow = Math.floor(
+    (timedays % (60 * 60 * 24 * 1000)) / (60 * 60 * 1000)
+  );
+  const minnow = Math.floor((timedays % (60 * 60 * 1000)) / (60 * 1000));
+  const secnow = Math.floor((timedays % (60 * 1000)) / 1000);
+  const timerCont = document.querySelector(".timer");
+  timerCont.innerHTML = `
+  <div class="hours d-flex flex-column ">
+            <span>${
+              hoursnow === 0 ? "00" : hoursnow >= 10 ? hoursnow : "0" + hoursnow
+            }</span>
+            <span>ساعات</span>
+          </div>
+          <div class="span">
+            <span>:</span>
+          </div>
+          <div class="minutes d-flex flex-column">
+            <span>${
+              minnow === 0 ? "00" : minnow >= 10 ? minnow : "0" + minnow
+            }</span>
+            <span>دقائق</span>
+          </div>
+          <div class="span">
+            <span>:</span>
+          </div>
+          <div class="seconds d-flex flex-column">
+            <span>${
+              secnow === 0 ? "00" : secnow >= 10 ? secnow : "0" + secnow
+            }</span>
+            <span>ثواني</span>
+          </div>`;
+}, 1000);
+
+const searchInput = document.getElementById("searchInput");
+const searchBar = document.querySelector(".header_searchbar");
+const productSearch = document.querySelector(".search__prodcut");
+searchInput.addEventListener("keyup", () => {
+  console.log(searchInput.value);
+  if (searchInput.value.trim().length == 0) {
+    productSearch.classList.add("hidden");
+  } else {
+    productSearch.classList.remove("hidden");
+  }
+ const newData= renderedProduct.filter(
+    (asd) =>
+      asd.name.ar.includes(searchInput.value) ||
+      asd.name.en.includes(searchInput.value)
+      );
+      productSearch.innerHTML= ""
+      if(newData.length === 0){
+        productSearch.innerHTML= `
+        <h2>لا يوجد</h2>
+        `
+
+      }else{
+
+        newData.forEach(e=>{
+      productSearch.innerHTML+=`
+      <div class=" d-flex gap-3 hoverMeNowHot align-items-center">
+      <div class="img">
+        <img src="${e.image[0]}" style="height: 40px;" >
+      </div>
+      <div class="title flex-grow-1">
+        <p>${e.name.ar}</p>
+      </div>
+      <div class="price">$${e.price}</div>
+    </div>`  })
+      }
+});
+window.addEventListener("click", (e) => {
+  console.log();
+  if (!e.target.closest(".header_searchbar")) {
+    productSearch.classList.add("hidden");
+  } else {
+    if (searchInput.value.trim().length !== 0) {
+      productSearch.classList.remove("hidden");
+      console.log("s");
+    }
+  }
+});
